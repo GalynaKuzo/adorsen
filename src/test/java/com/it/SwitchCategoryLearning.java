1ecee389ad7b952bb6f96f294ac307e9dc501f91
Galyna.Kuzo <galyna.kuzo@globallogic.com>
2019-03-19 10:52:09 +0200

    CommitTuesday

--- src/test/java/com/it/SwitchCategoryLearning.java
+++ src/test/java/com/it/SwitchCategoryLearning.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -22,14 +23,25 @@ public class SwitchCategoryLearning {
         ChromeDriver driver = new ChromeDriver();
 
         driver.get("https://ain.ua");
+
+        String block_title_ru1 = driver.findElement(By.cssSelector("#subbody > div.main-content > div > div:nth-child(3) > div.block-interesting > div.block-interesting-left > div > div")).getText();
+        Assert.assertEquals("ИНТЕРЕСНО СЕЙЧАС", block_title_ru1);
         WebElement element = driver.findElement(By.cssSelector("div > span.lang-switcher > label > ul > a"));
         element.click();
+        String block_title_eng = driver.findElement(By.cssSelector("#subbody > div.main-content > div > div:nth-child(2) > div.block-interesting > div.block-interesting-left > div > div")).getText();
+        Assert.assertEquals("TRENDING NOW", block_title_eng);
      /*  boolean switcher = driver.findElement(By.className("active")).isEnabled();
        Assert.assertEquals("active", switcher); */
         WebElement lang_switcher = driver.findElement(By.cssSelector("div > span.lang-switcher > label > ul > a"));
-        lang_switcher.click();   //todo add assertion to verify on rus/eng/rus pages
+        lang_switcher.click();
+        String block_title_ru2 = driver.findElement(By.cssSelector("#subbody > div.main-content > div > div:nth-child(3) > div.block-interesting > div.block-interesting-left > div > div")).getText();
+        Assert.assertEquals("ИНТЕРЕСНО СЕЙЧАС", block_title_ru2); //add assertion to verify on rus/eng/rus pages
+
         driver.findElement(By.cssSelector("#menu-tematika-1 > li.menu-item.menu-item-type-custom.menu-item-object-custom.menu-item-has-children.menu-item-711957 > a")).click(); //add assertion to dropdown is visible with elements
         driver.findElement(By.cssSelector("#menu-tematika-1 > li.menu-item.menu-item-type-custom.menu-item-object-custom.menu-item-has-children.menu-item-711957 > ul > li.menu-item.menu-item-type-taxonomy.menu-item-object-category.menu-item-743305 > a")).click(); //add assertion to right page is visible
+        String rinok_truda = driver.findElement(By.cssSelector("#subbody > div.main-content > div > div:nth-child(1) > div > h3")).getText();
+        Assert.assertEquals("Рынок труда", rinok_truda); //add assertion to verify Rinok truda page
+
         /*driver.findElement(By.cssSelector("#kps-custom-button > a > span.content-on-hover > span.text-content")).click();*/
         driver.get("https://ain.ua/cards/keep-calm-and-learn-english/");
         driver.manage().window().fullscreen();
